8ff9d6f9c0c254929654cbaada96b46a324541b2
zhaoxinpeng <15531506906@sina.cn>
2018-10-23 09:18:47 +0800

    需求管理 查看详情 展示分配给了谁

--- api/tests.py
+++ api/tests.py
@@ -6,25 +6,37 @@ from django.test import TestCase
 
 
 
-import socket
-# 获取本机 HOST
-host = socket.gethostname()
-port = 8000
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind((host,port))
-s.listen(1)
-sock,addr = s.accept()
-print('Connection built')
-info = sock.recv(1024).decode()
-while True:
-    print(sock)
-    print('李四:' + info)
-    send_mes = input()
-    # if send_mes:
-    print('====> ', send_mes)
-    sock.send(send_mes.encode())
-    # if send_mes =='exit':
-    #     break
-    info = sock.recv(1024).decode()
+import random
+# import socket
+# # 获取本机 HOST
+# host = socket.gethostname()
+# port = 8000
+# s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# s.bind((host,port))
+# s.listen(1)
+# sock,addr = s.accept()
+# print('Connection built')
+# info = sock.recv(1024).decode()
+# while True:
+#     print(sock)
+#     print('李四:' + info)
+#     send_mes = input()
+#     # if send_mes:
+#     print('====> ', send_mes)
+#     sock.send(send_mes.encode())
+#     # if send_mes =='exit':
+#     #     break
+#     info = sock.recv(1024).decode()
 # sock.close()
 # s.close()
+
+
+seed = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+sa = []
+for i in range(32):
+    sa.append(random.choice(seed))
+salt = ''.join(sa)
+print('salt------->',salt)
+
+import uuid
+str(uuid.uuid4()).replace('-', '')
